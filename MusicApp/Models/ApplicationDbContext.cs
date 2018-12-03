f58446406b5040fe2316f27c1eeead01eba3f507
Said Houwasse <said.houwasse@gmail.com>
2018-12-03 13:07:08 -0500

    Create concert model

---
+++ MusicApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace MusicApp.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Concert> Concerts { get; set; }
+        public DbSet<Genre> Genre { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
