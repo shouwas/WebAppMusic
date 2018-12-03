274385ac069c5ef05e5e67e1bba5f405e565c3d6
Said Houwasse <said.houwasse@gmail.com>
2018-12-03 13:56:48 -0500

    Add controller Concert

---
+++ MusicApp/Controllers/ConcertController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicApp.Controllers
+{
+    public class ConcertController : Controller
+    {
+        // GET: Concert
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
